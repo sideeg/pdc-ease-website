05350b6931927e1229736e50d7a3242c058e75ad
sideeg <Satelite902@gmail.com>
2020-10-26 11:35:25 +0200

    start artical blog controller

--- app/Http/Controllers/blogController.php
+++ app/Http/Controllers/blogController.php
@@ -4,17 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\blogs;
+use App\Models\tags;
+
 
 class blogController extends Controller
 {
     public function index()
     {
-        // return view('pages.blog');
-
-        //We will use datatable to display data by using index_data function
-        // $orders = Order::paginate(10);
-        // return view('orders.index', compact('orders'));
-        //$blog = json_encode( blog::orderBy('id', 'asc')->select('id','blog_name', 'Brief',"created_at","image")->get());
+        
        $blog = blogs::paginate(10);
         dd($blog);
         return view('pages.blog', compact('blog')); // TODO view name
@@ -23,12 +20,13 @@ class blogController extends Controller
 
     public function blogById( $id)
     {
-        // return view('pages.article');
-
+       
         // dd(json_encode( blog::find($id)));
        $blog = blogs::find($id);
+       // TODO get realted blogs and service tags
+       $tags = tags::all();
 
-        dd($blog);
+        dd($tags);
         return view("pages.article", compact('blog'));
     }
 }
