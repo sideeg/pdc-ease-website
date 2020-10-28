beefb794bea2128a5065e43335c1bad3255798b1
sideeg <Satelite902@gmail.com>
2020-10-28 09:35:10 +0200

    add message and tag request method

--- app/Http/Controllers/homeController.php
+++ app/Http/Controllers/homeController.php
@@ -66,5 +66,7 @@ class homeController extends Controller
 
         }
 
+        return redirect('/');
+
     }
 }
--- app/Http/Controllers/servicesController.php
+++ app/Http/Controllers/servicesController.php
@@ -6,5 +6,64 @@ use Illuminate\Http\Request;
 
 class servicesController extends Controller
 {
-    //
+    public function serviceRequest(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|max:255',
+            'phone' => 'required',
+            'email' => 'required',
+            'tags' => 'required',
+        ]);
+
+        $order = new Models\orders();
+        
+        $order->name = $request->name;
+        $order->email = $request->email;
+        $order->phone = $request->phone;
+
+        $order->save();
+
+        for ($i =0;i<sizeof($request['tags']);$i++){
+            $order_tag = new orders_tags();
+
+            $order_tag->order_id = $order->id;
+            $order_tag->tag_id = $request['tags'][i];
+
+            $order_tag->save();
+
+        }
+
+        return redirect('/service');
+
+    }
+
+    /***************************
+     * 
+     * 
+     */
+
+     public function tagRequest(Request $request)
+     {
+        $validatedData = $request->validate([
+            'name' => 'required|max:255',
+            'phone' => 'required',
+            'email' => 'required',
+            'tag_id' => 'required',
+        ]);
+
+        $order = new Models\orders();
+        
+        $order->name = $request->name;
+        $order->email = $request->email;
+        $order->phone = $request->phone;
+
+        $order->save();
+
+        $order_tag = new orders_tags();
+
+        $order_tag->order_id = $order->id;
+        $order_tag->tag_id = $request['tag_id'];
+
+        $order_tag->save();
+     }
 }
