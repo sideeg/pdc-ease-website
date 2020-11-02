db7244dc95655527bd7afbe6ea49affcb6ffaf60
Sohip <sohipbadawe@gmail.com>
2020-11-02 05:39:00 -0800

    10

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -5,11 +5,11 @@
             <div class="col-lg-12 text-center">
                 <div class="footer-menu ab-sm-15">
                     <ul class="list-unstyled social-icon mb-0">
-                        <li class="list-inline-item"><a href="#"><i class="fab fa-apple"></i></a></li>
-                        <li class="list-inline-item"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-                        <li class="list-inline-item"><a href="#"><i class="fab fa-dribbble"></i></a></li>
-                        <li class="list-inline-item"><a href="#"><i class="fab fa-instagram"></i></a></li>
-                        <li class="list-inline-item"><a href="#"><i class="fab fa-twitter"></i></a></li>
+                        {{-- <li class="list-inline-item"><a href="#"><i class="fab fa-apple"></i></a></li> --}}
+                        <li class="list-inline-item"><a href="https://web.facebook.com/EASE-Group-106816447870573" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+                        <li class="list-inline-item"><a href="Https://wa.me/+29911098025"><i class="fab fa-whatsapp"  target="_blank"></i></a></li>
+                        <li class="list-inline-item"><a href="https://www.instagram.com/easegroup_sd/"><i class="fab fa-instagram" target="_blank"></i></a></li>
+                        {{-- <li class="list-inline-item"><a href="#"><i class="fab fa-twitter" target="_blank"></i></a></li> --}}
                     </ul>
                     <p class="pt-4 pb-2"> &copy; 2010 - 2020 INISTORY ALL RIGHT RESERVED </p>
                 </div>
@@ -17,107 +17,6 @@
         </div>
     </div>
 </section>
-<!-- FOOTER END -->
-<!-- FOOTER START -->
-{{-- <footer class="footer bg-dark">
-    <div class="section-two">
-        <div class="container"> --}}
-            <!--Footer Info -->
-            <!-- <div class="row footer-info">
-                <div class="col-lg-3 col-md-12 col-sm-12">
-                    <div class="">
-                        <a href="index-2.html" class="logo">
-                            <img src="images/logo/ease.png" alt="missing_logo" height="20">
-                        </a>
-                    </div>
-                    <p class="text-footer-clr mt-3">These cases are perfectly simple and easy to free hour, when nothing prevents distinguish.</p>
-                    <div>
-                        <ul class="list-unstyled social-icon">
-                            <li class="list-inline-item"><a href="#"><i class="fab fa-apple"></i></a></li>
-                            <li class="list-inline-item"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-                            <li class="list-inline-item"><a href="#"><i class="fab fa-dribbble"></i></a></li>
-                            <li class="list-inline-item"><a href="#"><i class="fab fa-instagram"></i></a></li>
-                            <li class="list-inline-item"><a href="#"><i class="fab fa-twitter"></i></a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-lg-2 col-md-4 col-sm-4">
-                    <div class="footer-head">
-                        <h5 class="text-light">Company</h5>
-                    </div>
-                    <div class="footer-item mt-3">
-                        <ul class="list-unstyled">
-                            <li><a href="#"><i class="fas fa-chevron-right mr-2"></i> About us</a></li>
-                            <li><a href="#"><i class="fas fa-chevron-right mr-2"></i> Services</a></li>
-                            <li><a href="#"><i class="fas fa-chevron-right mr-2"></i> Portfolio</a></li>
-                            <li><a href="#"><i class="fas fa-chevron-right mr-2"></i> News</a></li>
-                            <li><a href="#"><i class="fas fa-chevron-right mr-2"></i> FAQ</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-lg-2 col-md-4 col-sm-4">
-                    <div class="footer-head">
-                        <h5 class="text-light">Useful</h5>
-                    </div>
-                    <div class="footer-item mt-3">
-                        <ul class="list-unstyled">
-                            <li><a href="#"><i class="fas fa-chevron-right mr-2"></i> Accounts</a></li>
-                            <li><a href="#"><i class="fas fa-chevron-right mr-2"></i> Sales & Support</a></li>
-                            <li><a href="#"><i class="fas fa-chevron-right mr-2"></i> Orders Track</a></li>
-                            <li><a href="#"><i class="fas fa-chevron-right mr-2"></i> Download Center</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-lg-2 col-md-4 col-sm-4">
-                    <div class="footer-head">
-                        <h5 class="text-light">Additional</h5>
-                    </div>
-                    <div class="footer-item mt-3">
-                        <ul class="list-unstyled">
-                            <li><a href="#"><i class="fas fa-chevron-right mr-2"></i> Portfolio</a></li>
-                            <li><a href="#"><i class="fas fa-chevron-right mr-2"></i> Contact Us</a></li>
-                            <li><a href="#"><i class="fas fa-chevron-right mr-2"></i> Blog</a></li>
-                            <li><a href="#"><i class="fas fa-chevron-right mr-2"></i> Help Center</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-12">
-                    <div class="footer-head">
-                        <h5 class="text-light">Newsletter</h5>
-                    </div>
-                    <div class="footer-news mt-3">
-                        <p class="text-footer-clr">Subscribe to our email newsletter to receive useful articles and special offers.</p>
-                        <div class="subscribed-form">
-                            <div class="subcribed-form">
-                                <form action="#">
-                                    <input name="email" type="email" placeholder="Your Email" id="email" required="">
-                                    <button type="submit" class=""><span class="fab fa-telegram-plane"></span></button>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div> -->
-            <!-- End Footer Info -->
-        {{-- </div>
-    </div> --}}
-    {{-- <hr> --}}
-
-    <!-- Copyright Bar -->
-    <!-- <section class="section-30">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-lg-12">
-                    <div class="text-center">
-                        <p class="copy-rights text-white mb-0"><a href="https://www.templatespoint.net" target="_blank">Templates Point</a></p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section> -->
-    <!-- End Copyright Bar -->
-{{-- </footer> --}}
-<!-- FOOTER END -->
 
 <!-- Back to top -->    
 <!-- <a href="#" class="back-to-top" id="back-to-top"> 
