34c257453113a6af6fff33c7714165227aa0c5fb
omaraladin <48786827+omaraladin@users.noreply.github.com>
2020-11-06 00:57:34 +0200

    Kind change in Social Media Links
    I kindly, changed the social media links of Instagram and WhastApp

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -48,8 +48,8 @@ toastr.error(document.getElementById('contactfailToast').innerText);
                     <ul class="list-unstyled social-icon mb-0">
                         {{-- <li class="list-inline-item"><a href="#"><i class="fab fa-apple"></i></a></li> --}}
                         <li class="list-inline-item"><a href="https://web.facebook.com/EASE-Group-106816447870573" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
-                        <li class="list-inline-item"><a href="Https://wa.me/+29911098025" target="_blank"><i class="fab fa-whatsapp" ></i></a></li>
-                        <li class="list-inline-item"><a href="https://www.instagram.com/easegroup_sd/" target="_blank"><i class="fab fa-instagram"></i></a></li>
+                        <li class="list-inline-item"><a href="https://wa.me/message/WD3CZLXRQWMCA1" target="_blank"><i class="fab fa-whatsapp" ></i></a></li>
+                        <li class="list-inline-item"><a href="https://instagram.com/easegroupsd" target="_blank"><i class="fab fa-instagram"></i></a></li>
                         <li class="list-inline-item"><a href="https://twitter.com/easegroupsd?s=08" target="_blank"><i class="fab fa-twitter"></i></a></li>
                     </ul>
                     <p class="pt-4 pb-2"> &copy; 2010 - 2020 ALL RIGHT RESERVED </p>
